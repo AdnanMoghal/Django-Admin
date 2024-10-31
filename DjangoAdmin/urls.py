1eb16146d0a4e7c26fc3a5e1ce6c0f8aedc6abdf
Adnan Moghal <theadnanmoghal@gmail.com>
2024-10-31 20:10:02 +0500

    add homepage redirection

--- DjangoAdmin/urls.py
+++ DjangoAdmin/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.shortcuts import redirect
 from django.urls import path, include
 from django.contrib.auth import views as auth_views  # Import auth_views here
 
@@ -23,4 +24,5 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),  # Include the accounts app URLs
     path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('', lambda request: redirect('admin/login/')),  # Redirect root to login
 ]
