9ee5fab197efcc04afa10efe61987c2728e7e57a
Adnan Moghal <theadnanmoghal@gmail.com>
2024-11-07 17:44:03 +0500

    delete extra files

--- DjangoAdmin/settings.py
+++ DjangoAdmin/settings.py
@@ -44,9 +44,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'accounts',
+    # 'accounts',
     'tailwind',
-    'theme',
+    # 'theme',
     'django_browser_reload'
 ]
 
