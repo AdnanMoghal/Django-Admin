875212ddae7f12d24d908c4e0ec7211383697f6a
Adnan Moghal <theadnanmoghal@gmail.com>
2024-11-14 17:09:22 +0500

    remove extra code

--- DjangoAdmin/settings.py
+++ DjangoAdmin/settings.py
@@ -44,10 +44,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'accounts',
-    'tailwind',
-    # 'theme',
-    'django_browser_reload'
 ]
 
 MIDDLEWARE = [
@@ -66,7 +62,7 @@ ROOT_URLCONF = 'DjangoAdmin.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
